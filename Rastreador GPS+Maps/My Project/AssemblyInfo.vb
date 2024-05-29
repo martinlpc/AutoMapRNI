92e63910a563f02518bd17d5980e7069e366bda5
martinlpc <martinlpacheco@gmail.com>
2024-05-29 15:42:37 -0300

    Edit uncertainty files for NBM550 probes
    Se ajustaron los valores de incertidumbre y factor en los archivos de sonda del NBM550 según los nuevos cálculos de HDJ

--- Rastreador GPS+Maps/My Project/AssemblyInfo.vb
+++ Rastreador GPS+Maps/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.13.10")> 
-<Assembly: AssemblyFileVersion("1.13.10")> 
+<Assembly: AssemblyVersion("1.13.11")> 
+<Assembly: AssemblyFileVersion("1.13.11")> 
